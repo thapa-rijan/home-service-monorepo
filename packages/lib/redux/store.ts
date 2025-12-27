24818f65db4802f09e6f26959621b08d681f9d46
thapa-rijan <dev.rijanthapa@gmail.com>
2025-12-27 07:24:28 +0545

    feat: login page

---
+++ packages/lib/redux/store.ts
@@ -0,0 +1,30 @@
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from "redux";
+import createSagaMiddleware from "redux-saga";
+import rootReducer, { RootState } from "./reducers/index";
+import rootSaga from "./sagas/index";
+
+// Create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Enable Redux DevTools in development
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+// Create the store
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware))
+);
+
+// Run the root saga
+sagaMiddleware.run(rootSaga);
+
+export type AppDispatch = typeof store.dispatch;
+export type { RootState };
+export default store;
