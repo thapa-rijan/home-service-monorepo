d6eb42e957b802df3e32fa5b6bf0ea8d7f5b5c60
thapa-rijan <dev.rijanthapa@gmail.com>
2026-01-02 11:43:11 +0545

    feat:  fest user api

--- packages/lib/redux/actions/auth.actions.ts
+++ packages/lib/redux/actions/auth.actions.ts
@@ -7,37 +7,7 @@ import {
   SET_AUTH_TOKEN,
   CLEAR_AUTH,
 } from "../constants/auth.constants";
-
-// Types
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-  role: string;
-  number: string;
-  address: string;
-  jobTitle: string | null;
-  skills: string | null;
-  experienceYears: number | null;
-  workingHours: string | null;
-  availableDays: string | null;
-  hourlyRate: number | null;
-  languages: string | null;
-  bio: string | null;
-  isActive: boolean;
-  createdAt: string;
-  updatedAt: string;
-}
-
-export interface LoginCredentials {
-  email: string;
-  password: string;
-}
-
-export interface LoginResponse {
-  accessToken: string;
-  user: User;
-}
+import { User, LoginCredentials, LoginResponse } from "../interfaces";
 
 // Action Interfaces
 export interface LoginRequestAction {
