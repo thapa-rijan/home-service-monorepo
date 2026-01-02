06344be282398a5d05f91a9bde43130b491b59e4
thapa-rijan <dev.rijanthapa@gmail.com>
2026-01-02 11:43:49 +0545

    feat:  fest user api

--- packages/lib/redux/reducers/index.ts
+++ packages/lib/redux/reducers/index.ts
@@ -1,14 +1,20 @@
 import { combineReducers } from "redux";
-import authReducer, { AuthState } from "./auth.reducer";
+import authReducer from "./auth.reducer";
+import staffReducer from "./staff.reducer";
+import userReducer from "./user.reducer";
+import { AuthState, StaffState, UserState } from "../interfaces";
 
 export interface RootState {
   auth: AuthState;
+  staff: StaffState;
+  user: UserState;
   // Add more reducers here
 }
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  // Add more reducers here
+  staff: staffReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
