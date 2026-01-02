9c62326da0f12673a63dd55dc84b24a321037b9b
thapa-rijan <dev.rijanthapa@gmail.com>
2026-01-02 12:27:51 +0545

    feat: remove commits

--- packages/lib/redux/sagas/index.ts
+++ packages/lib/redux/sagas/index.ts
@@ -2,13 +2,9 @@ import { all, fork } from "redux-saga/effects";
 import authSaga from "./auth.saga";
 import staffSaga from "./staff.saga";
 
-// Root Saga
 export default function* rootSaga() {
   yield all([
     fork(authSaga),
     fork(staffSaga),
-    // Add more sagas here as your application grows
-    // fork(userSaga),
-    // fork(productSaga),
   ]);
 }
