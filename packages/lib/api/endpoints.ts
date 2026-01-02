5253e2a7fd7e8f757517341d3b6ea38a0114d83a
thapa-rijan <dev.rijanthapa@gmail.com>
2026-01-02 11:43:03 +0545

    feat:  fest user api

--- packages/lib/api/endpoints.ts
+++ packages/lib/api/endpoints.ts
@@ -10,6 +10,7 @@ export const API_ENDPOINTS = {
     RESET_PASSWORD: "/auth/reset-password",
     VERIFY_EMAIL: "/auth/verify-email",
     CHANGE_PASSWORD: "/auth/change-password",
+    STAFF: "/auth/staff",
   },
 
   // User endpoints
